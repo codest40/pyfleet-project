2cf63b6e94fd56cda740d63b4c79b3007c5af3f7
codest40 <generalcatmouse@gmail.com>
2026-01-15 15:06:33 +0100

    New Commit on 2026_01_15_15_06_33

--- terra/modules/db/main.tf
+++ terra/modules/db/main.tf
@@ -1,8 +1,8 @@
 # DB Security Group
 resource "aws_security_group" "db_sg" {
   description = "Security group for PostgreSQL database"
-  name   = "pyfleet-db-sg"
-  vpc_id = var.vpc_id
+  name        = "pyfleet-db-sg"
+  vpc_id      = var.vpc_id
 
   # Allow inbound Postgres traffic ONLY from App SG
   ingress {
@@ -32,19 +32,19 @@ resource "aws_db_subnet_group" "db_subnets" {
 }
 
 resource "aws_db_instance" "primary" {
-  identifier             = "pyfleet-db-primary"
-  allocated_storage      = var.db_allocated_storage
-  engine                 = "postgres"
-  engine_version         = var.engine_version
-  instance_class         = var.db_instance_class
-  username               = var.db_username
-  password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-  multi_az               = var.multi_az
+  identifier                   = "pyfleet-db-primary"
+  allocated_storage            = var.db_allocated_storage
+  engine                       = "postgres"
+  engine_version               = var.engine_version
+  instance_class               = var.db_instance_class
+  username                     = var.db_username
+  password                     = var.db_password
+  db_subnet_group_name         = aws_db_subnet_group.db_subnets.name
+  vpc_security_group_ids       = [aws_security_group.db_sg.id]
+  multi_az                     = var.multi_az
   performance_insights_enabled = true
-  publicly_accessible    = false
-  skip_final_snapshot    = true
+  publicly_accessible          = false
+  skip_final_snapshot          = true
 
   # Enable automated backups
   backup_retention_period = 7
@@ -52,17 +52,17 @@ resource "aws_db_instance" "primary" {
 }
 
 resource "aws_db_instance" "replicas" {
-  count                  = var.replica_count
-  identifier             = "pyfleet-db-replica-${count.index + 1}"
-  instance_class         = var.db_instance_class
-  engine                 = "postgres"
-  engine_version         = var.engine_version
-  db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-  publicly_accessible    = false
-  replicate_source_db    = aws_db_instance.primary.arn
+  count                        = var.replica_count
+  identifier                   = "pyfleet-db-replica-${count.index + 1}"
+  instance_class               = var.db_instance_class
+  engine                       = "postgres"
+  engine_version               = var.engine_version
+  db_subnet_group_name         = aws_db_subnet_group.db_subnets.name
+  vpc_security_group_ids       = [aws_security_group.db_sg.id]
+  publicly_accessible          = false
+  replicate_source_db          = aws_db_instance.primary.arn
   performance_insights_enabled = true
-  skip_final_snapshot    = true
+  skip_final_snapshot          = true
 
   tags = merge(var.tags, { Role = "replica-${count.index + 1}" })
 }
